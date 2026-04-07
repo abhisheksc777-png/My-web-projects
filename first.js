1d8d9e7ccfe84139c9727cb8d6e263cc8797df70
Abhishek Singh Chawada <abhisheksc777@gmail.com>
2026-04-07 17:50:13 +0530

    Delete first.js

--- first.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('video[controls]').forEach(vid => {
-    vid.controlsList.add('nodownload');  // Adds safely
-  });
-});
